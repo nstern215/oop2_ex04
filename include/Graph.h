d8476dec252dbfa4d608049c267d9f3288531238
Netanel Stern <netanel@ezsave.co.il>
2022-05-05 00:16:04 +0300

    working iterator for graph

--- include/Graph.h
+++ include/Graph.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <memory>
 #include <map>
 #include "Node.h"
@@ -10,6 +11,27 @@ class Graph
 public:
 	void insert(T1 id, T2 node);
 	void addEdge(T1 id1, T1 id2);
+
+	
+	friend struct Iterator;
+
+	struct Iterator
+	{
+		Iterator(std::map < T1, std::shared_ptr<Node<T2>>>::iterator iterator) :m_iterator(iterator) {}
+		Node<T2>& operator*()	{ return *(m_iterator->second); }
+		Node<T2>* operator->()	{ return m_iterator->second.get(); }
+		Iterator operator++()	{ return ++m_iterator; }
+		bool operator==(const Iterator& other) const { return m_iterator == other.m_iterator; }
+		bool operator!=(const Iterator& other) const { return !(m_iterator == other.m_iterator); }
+	private:
+		std::map < T1, std::shared_ptr<Node<T2>>>::iterator m_iterator;
+	};
+
+	Iterator begin()	{ return Iterator(m_nodes.begin()); }
+	Iterator end()		{ return Iterator(m_nodes.end()); }
+	
+	const Node<T2>& operator[](T1 id) const;
+
 private:
 	std::map<T1, std::shared_ptr<Node<T2>>> m_nodes;
 };
@@ -28,5 +50,11 @@ void Graph<T1, T2>::addEdge(T1 id1, T1 id2)
 
 	m_nodes.at(id1)->addNeighbor(m_nodes.at(id2));
 	m_nodes.at(id2)->addNeighbor(m_nodes.at(id1));
+}
+
+template <typename T1, typename T2>
+const Node<T2>& Graph<T1, T2>::operator[](T1 id) const
+{
+	return *m_nodes.at(id);
 }
 
--- include/Node.h
+++ include/Node.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <unordered_set>
 #include <memory>
 
@@ -7,11 +8,20 @@ class Node
 {
 public:
 	Node(T data = {});
+	Node(const Node& other);
 	void addNeighbor(std::shared_ptr<Node<T>> node);
+	T data() const;
 private:
 	std::unordered_set<std::shared_ptr<Node<T>>> m_neighbors;
 	T m_data;
 };
+
+template <typename T>
+Node<T>::Node(const Node& other):
+	m_data(other.data())
+{
+	std::cout << "copy ctor" << std::endl;
+}
 
 template <typename T>
 Node<T>::Node(T data):
@@ -22,4 +32,10 @@ template <typename T>
 void Node<T>::addNeighbor(std::shared_ptr<Node<T>> node)
 {
 	m_neighbors.insert(node);
+}
+
+template <typename T>
+T Node<T>::data() const
+{
+	return m_data;
 }
